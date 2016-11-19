3e3ea2ddb9d54a8e9a11ed6abeb399288f1f920f
Arek Gorzawski <agorzawski@gmail.com>
2016-11-19 13:19:17 +0100

    updated version. v1

--- app/src/main/java/arek/carcostsharing/MainScreen.java
+++ app/src/main/java/arek/carcostsharing/MainScreen.java
@@ -1,17 +1,32 @@
 package arek.carcostsharing;
 
-import android.icu.text.DecimalFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class MainScreen extends AppCompatActivity {
 
+    private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("###.#");
+    private static final double DISTANCE_IN_KM_AT_WHICH_DIFFERENT_VALUE_PER_KM = 200;
+    private static final double VALUE_PER_KM_BELOW_BORDER_DISTANCE = 0.1;
+    private static final double VALUE_PER_KM_ABOVE_BORDER_DISTANCE = 0.05;
+
+
     private Button calculate;
+
     private EditText peopleInTrip;
-    private EditText pricePerPerson;
+    private EditText distance;
+    private EditText carsInTrip;
+    private EditText tollPerCar;
+
+    private TextView pricePerPerson;
+    private TextView pricePerCar;
+    private TextView valuePerPerson;
 
 
     @Override
@@ -20,41 +35,53 @@ public class MainScreen extends AppCompatActivity {
         setContentView(R.layout.activity_main_screen);
 
         calculate = (Button) findViewById(R.id.calculate);
+
         peopleInTrip = (EditText) findViewById(R.id.nbOfPeople);
-        pricePerPerson = (EditText) findViewById(R.id.pricePerPerson);
+        carsInTrip = (EditText) findViewById(R.id.numberOfCars);
+        distance = (EditText) findViewById(R.id.totalDistance);
+        tollPerCar = (EditText) findViewById(R.id.tollPerCar);
+
+        pricePerPerson = (TextView) findViewById(R.id.costPerPerson);
+        pricePerCar = (TextView) findViewById(R.id.costPerCar);
+        valuePerPerson = (TextView) findViewById(R.id.valuePerPerson);
 
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                double distanceInKm = 150;
-
-                double factor  = (distanceInKm<200 ? 1 : 0.5);
-
-                double valuePerPersonForDistance = distanceInKm * factor;
+                double distanceInKm = Double.valueOf(distance.getText().toString());
+                double valuePerPersonForDistance = getValueForDistance(distanceInKm);
+                valuePerPerson.setText(DECIMAL_FORMAT.format(valuePerPersonForDistance));
 
                 int nbOfPeopleInTrip = Integer.valueOf(peopleInTrip.getText().toString());
 
                 double totalValueForAllPeoples = nbOfPeopleInTrip * valuePerPersonForDistance;
 
-                double totalCars = 2;
+                double totalCars = Integer.valueOf(carsInTrip.getText().toString());
+                double additionalExpensPerCar = Double.valueOf(tollPerCar.getText().toString());
 
-                double additionalExpensPerCar = 0;
-
-                double totalCarExpenses = totalCars*additionalExpensPerCar;
-
-
+                double totalCarExpenses = totalCars * additionalExpensPerCar;
                 double totalCost = totalCarExpenses + totalValueForAllPeoples;
 
-                double personShare = totalCarExpenses / (nbOfPeopleInTrip);
-                double carShare = totalCarExpenses / totalCars;
-                java.text.DecimalFormat df = new java.text.DecimalFormat("#.##");
+                double personShare = totalCost / (nbOfPeopleInTrip - 1);
+                double carShare = totalCost / totalCars;
 
-                pricePerPerson.setText(df.format(personShare));
+                pricePerPerson.setText(DECIMAL_FORMAT.format(personShare));
+                pricePerCar.setText(DECIMAL_FORMAT.format(carShare));
 
             }
         });
 
+    }
 
+    public double getValueForDistance(double distance) {
+        double value = 0;
+        if (distance < DISTANCE_IN_KM_AT_WHICH_DIFFERENT_VALUE_PER_KM) {
+            value = distance * VALUE_PER_KM_BELOW_BORDER_DISTANCE;
+        } else {
+            value = DISTANCE_IN_KM_AT_WHICH_DIFFERENT_VALUE_PER_KM * VALUE_PER_KM_BELOW_BORDER_DISTANCE +
+                    (distance - DISTANCE_IN_KM_AT_WHICH_DIFFERENT_VALUE_PER_KM) * VALUE_PER_KM_ABOVE_BORDER_DISTANCE;
+        }
+        return value;
     }
 }
